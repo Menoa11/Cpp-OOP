7f732e8d3d28353fce639dc2bb1729352bea913f
Menoa <brennan.menoa@gmail.com>
2025-01-12 15:15:30 -0800

    Adding the basics for the cpu class and main function

---
+++ cpu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+class CPU {
+
+private:
+    int temperature;
+    int usage; 
+
+
+public:
+    int getUsage(){
+        return usage;
+    }
+
+    
+};
